f2b225af90349af3e0b4875d350d7ba0c22c798e
Yahyayev Kenan <yahyayevkenan57@gmail.com>
2025-12-09 23:25:52 +0400

    Update 9-hoisting.js

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,17 +1,19 @@
-class HolbertonClass {
-  constructor(year, location) {
-    this._year = year;
-    this._location = location;
+class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
   }
 
-  get year() {
-    return this._year;
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
   }
 
-  get location() {
-    return this._location;
+  get holbertonClass() {
+    return this._holbertonClass; // ✅ fixed
   }
 }
+
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
